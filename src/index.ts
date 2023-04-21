10672c039abacfe00dfdec98fa59ec88a89e0421
AndreNovaisBrito <andrenoz@gmail.com>
2023-04-21 10:48:57 -0300

    feat: users repository

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { MongoGetUsersRepository } from "./repositories/mongo-get-users";
+import { GetUsersController } from "./controllers/get-users/get-users";
 import express from "express";
 import { config } from "dotenv";
 
@@ -9,6 +11,10 @@ const port = process.env.PORT || 8000;
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
 
-app.get("/", (req, res) => {
-  res.send("hello world");
+app.get("/users", async (req, res) => {
+  const mongoGetUsersRepository = new MongoGetUsersRepository();
+  const getUsersController = new GetUsersController(mongoGetUsersRepository);
+  const { body, statusCode } = await getUsersController.handle();
+
+  res.send(body).status(statusCode);
 });
